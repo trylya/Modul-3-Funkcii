508d2291418764aa3df34b267b0c987c53324680
Лена <trylya@mail.ru>
2024-09-27 11:34:22 +0300

    способы вызова функций

--- module_3_2.py
+++ module_3_2.py
@@ -1,11 +1,11 @@
 def send_email (message, recipient, sender = 'universiry.help@gmail.com'):
-    if (("@" and ('.com' or '.ru' or '.net')) not in recipient
-            and ('@' and ('.com' or '.ru' or '.net')) not in sender):
-        print ("Невозможно отправить письмо с адреса", sender,  'на адрес', recipient)
+    if sender == recipient:
+        print("Нельзя отправить письмо самому себе")
     elif sender == 'universiry.help@gmail.com':
         print ('Письмо успешно отправлено с адреса' , sender, 'на адрес', recipient)
-    elif sender == recipient:
-        print("Нельзя отправить письмо самому себе")
+    elif (("@" and ('.com' or '.ru' or '.net')) not in recipient
+            and ('@' and ('.com' or '.ru' or '.net')) not in sender):
+        print ("Невозможно отправить письмо с адреса", sender,  'на адрес', recipient)
     else:
         print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса ', sender, 'на адрес', recipient)
 send_email("Это сообщение для проверки связи ",  'vasek1574@gmail.com')
